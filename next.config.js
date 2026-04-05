e2b6ea6306b95556784a3a11466732eba802f826
your_username <saadchadu@gmail.com>
2026-04-05 11:45:03 +0530

    fix: add image quality values 80 and 85 to next.config

--- next.config.js
+++ next.config.js
@@ -45,6 +45,7 @@ const nextConfig = {
   },
   images: {
     formats: ['image/webp', 'image/avif'],
+    qualities: [75, 80, 85],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000, // 1 year
